56e1a80a19c0a058582ed65e84e7653ba8e8a832
diegosorace <dsorace@prismamp.com>
2023-01-24 12:22:56 -0300

    update

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "fargate_cluster_name" {
   description = "ECS cluster name"
   value       = aws_ecs_cluster.main.name
 }
+
+output "fargate_cluster_arn" {
+  description = "ECS cluster name"
+  value       = aws_ecs_cluster.main.arn
+}
